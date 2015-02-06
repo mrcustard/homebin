6418b1865ec2459bf28f94da2e30617f3d43943f
Mark Gibson <mark.gibson@turner.com>
2015-02-06 10:21:56 -0500

    Adding config file for fakeip so that you can specify network

--- fakeip.rb
+++ fakeip.rb
@@ -1,9 +1,21 @@
 #!/usr/bin/env ruby
 # This script will build a fake ip address for use in testing
 
-r = Random.new
-(1..100).each do |n|
-  puts "10.50.0.#{r.rand(0..254)}"
+require 'json'
+
+$config = "#{ENV['HOME']}/.fakeip.conf" # Set the network, should be first 3 oct.
+$network = JSON.parse(File.open($config, 'r').read)['network']
+
+def generateip()
+  r = Random.new
+  return "#{$network}.#{r.rand(0..254)}"
+end
+
+if __FILE__ == $0
+  r = Random.new
+  (1..100).each do |n|
+    puts "#{$network}.#{r.rand(0..254)}"
+  end
 end
 
 
