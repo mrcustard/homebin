01296c5bc5257c20678f7bf1c3390ad1e823e7ba
Mark Gibson <mark.gibson@turner.com>
2015-09-10 02:47:39 -0400

    making changes and fixing scripts

--- circonus_tools/get-all-checks.rb
+++ circonus_tools/get-all-checks.rb
@@ -4,6 +4,11 @@ require 'json'
 require 'rest_client'
 require 'circonus'
 
+require_relative './lib/getcpu'
+require_relative './lib/getmem'
+require_relative './lib/getdisk'
+require_relative './lib/getnodeinfo'
+
 f = open('./config.json', 'r').read
 @j = JSON.parse(f)
 
@@ -18,31 +23,16 @@ hours = @j['hours'] # Change this in the config.json file
 @endtime = Time.now.to_i # get the current time and convert to epoch
 @starttime = @endtime - (hours * 60 * 60) # convert hours to seconds and subtract from the current time epoch
 
-puts "%-37s, %-20s, %-20s, %-20s, %-20s, %-20s, %-20s, %-20s" % %w{ Hostname Customer CPUMeanIdle CPUPeakIdle MemMeanUsed MemPeakUsed DiskMeanUsed DiskPeakUsed }
-def data(hostname, customer,  checkid)
-  c = Circonus.new('a06a1a30-4249-4ec3-bb0b-e54c890a7f15', 'curl')
-  c.set_server('api.mon.vgtf.net')
-  
-  cpulist = []
-  memlist = []
-  disklist = []
-  begin 
-    c.get_data(checkid, @j['cpu'], {'start' => @starttime, 'end' => @endtime})['data'].each do |line|
-      cpulist << line[1]['counter']
-    end
-    
-    c.get_data(checkid, @j['memory'], {'start' => @starttime, 'end' => @endtime})['data'].each do |line|
-      memlist << line[1]['value']
-    end
-    
-    c.get_data(checkid, @j['disk'], {'start' => @starttime, 'end' => @endtime})['data'].each do |line|
-      disklist << line[1]['value']
-    end
-    
-    puts "%-37s, %-20s, %-20s, %-20s, %-20s, %-20s, %-20s, %-20s" % [hostname, customer, (cpulist.reduce(&:+) / cpulist.length).round(2).to_s, cpulist.sort![-1].round(2).to_s, ((memlist.reduce(&:+) / memlist.length) / 1024 / 1024 ).round(2).to_s, (memlist.sort![-1] / 1024 / 1024).round(2).to_s, ((disklist.reduce(&:+) / disklist.length) / 1024 / 1024).round(2).to_s, (disklist.sort![-1] / 1024 / 1024).round(2).to_s ]
-  rescue 
-    puts "%-37s, %-20s, %-20s, %-20s, %-20s, %-20s, %-20s, %-20s" % [hostname, customer, 'nil', 'nil', 'nil', 'nil', 'nil', 'nil']
-  end
+@c = Circonus.new('a06a1a30-4249-4ec3-bb0b-e54c890a7f15', 'curl')
+@c.set_server('api.mon.vgtf.net')
+
+puts "%-37s %-20s %-20s %-20s %-20s %-20s %-20s %-20s" % %w{ Hostname Customer PackageSize AvgCPUutilized PeakCPUutilzed AVGMemUsed PeakMemUsed AVGDiskUsed PeakDiskUsed }
+def create_report(hostname, customer, checkid)
+  peakcpu_utilized, avgcpu_utilzed = get_cpu_utilization(checkid)
+  peakmem, avgmem = get_memory(checkid)
+  peakdisk, avgdisk = get_disk_info(checkid)
+  package_size = idbquery(hostname, 'package_size')
+  puts "%-37s, %-20s, %-20s, %-20s, %-20s, %-20s, %-20s, %-20s" % [hostname, customer, package_size, avgcpu_utilzed, peakcpu_utilized, avgmem, peakmem, avgdisk, peakdisk ]
 end
 
 begin
@@ -60,13 +50,13 @@ begin
           customer = t.split(":")[1]
           checkid_tmp = r['_checks'].map { |x| x.gsub('/check/', '')}
           checkid = checkid_tmp[0]
+          puts "got host"
           #printf("|%-40s|%-20s|%s\n", hostname, customer, checkid)
-          data(hostname, customer, checkid)
+          create_report(hostname, customer, checkid)
         end
       end 
     end
   end
-
 rescue RestClient::Exception => ex
   # deal with exceptions by accessing the returned json and printing out why
   result = JSON.parse(ex.http_body)
